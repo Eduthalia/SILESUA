c6896f596a06acd4c8ecd746daf6457dc2bbf29c
Edom Alemayehu <121560319+Eduthalia@users.noreply.github.com>
2024-04-05 00:49:48 +0300

    panic button

---
+++ panic/android/app/src/main/kotlin/com/example/panic/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.panic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ panic/lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AlertPage(),
+    );
+  }
+}
+
+class AlertPage extends StatefulWidget {
+  @override
+  _AlertPageState createState() => _AlertPageState();
+}
+
+class _AlertPageState extends State<AlertPage> {
+  bool _isButtonEnabled = false;
+  bool _alertSent = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Silesua',
+          style: TextStyle(color: Color(0xFFDB29BF)),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFCE4EC), // Very light pink color
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'Hold for three seconds to send distress call',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xFFDB29BF),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onLongPress: () {
+                Future.delayed(Duration(seconds: 3), () {
+                  setState(() {
+                    _isButtonEnabled = true;
+                  });
+                  _sendAlert();
+                });
+              },
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Color(0xFFDB29BF), // Silesua color
+                  borderRadius: BorderRadius.circular(75), // Make it a circle
+                ),
+                child: Center(
+                  child: Text(
+                    'SOS',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _alertSent ? null : () => _sendAlert(),
+        tooltip: 'Send Alert',
+        child: Icon(Icons.warning),
+        backgroundColor: _alertSent ? Colors.grey : Colors.red,
+      ),
+    );
+  }
+
+  void _sendAlert() {
+    setState(() {
+      _alertSent = true;
+    });
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Alert'),
+          content: Text('Alert successfully sent'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
